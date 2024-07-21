f05f30757aabcfecf069cd8db2275104b05110e2
JihyeokJung <jihyuk2291@naver.com>
2024-07-21 18:28:43 +0900

    2024.07.21(SUN)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 from SystemModel import outage_probability
 from reproduce import outage_prob_plot
 
+from scipy.linalg import svd
+
 '''********************* hyperparameter  *********************'''
 Omega_S = 1  # Variance of h_SI (channel for Source - RIS)
 Omega_I = 0.5  # Variance of h_ID (channel for RIS-Destination)
@@ -52,8 +54,6 @@ def outage_prob_simulation():
     np.save(f"./Curves/outage_prob_{2}_{8}", np.array([narrow_rho_values, P_out_values_2_8]))
 
 
-outage_prob_simulation()
-outage_prob_plot()
 '''
 # print(narrow_rho_values, 10*np.log10(narrow_rho_values))
 
@@ -62,3 +62,30 @@ outage_prob_plot()
   --> when iter 2e+7, result is 0.0
   --> when iter 5e+7, result is 4e-8
 '''
+
+def generate_opt_BD_phase():
+    S = h2.conj().T @ h1.conj().T + h1 @ h2
+
+    # Takagi factorization
+    U, Sigma, Vh = svd(S)
+    V1 = U
+    V2 = np.diag(Sigma) @ Vh
+
+    V = V1 @ V2
+    phi_opt = V @ V.T
+
+    return phi_opt
+
+def generate_opt_sc_phase():
+    # 각 채널 벡터의 위상 추출
+    theta_1 = np.angle(h1)
+    theta_2 = np.angle(h2)
+
+    # 최적의 위상 계산
+    phi_opt = - (theta_1 + theta_2.T)
+
+    return phi_opt
+
+'''********************* Run  *********************'''
+outage_prob_simulation()
+outage_prob_plot()
